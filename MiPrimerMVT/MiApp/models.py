13312290f825514f75e2bfc5f356024cc25a355a
agustinbuisan <agustin.buisan@gmail.com>
2022-05-01 23:50:53 -0300

    Created model and migrated to DB

--- MiPrimerMVT/MiApp/models.py
+++ MiPrimerMVT/MiApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Familiar(models.Model):
+    nombre= models.CharField(max_length=20)
+    apellido= models.CharField(max_length=20)
+    edad= models.IntegerField()
+    fecha_nacimiento= models.DateField()
+    mail=models.EmailField()
+    rol_familiar= models.CharField(max_length=20)
